ea4cf9408af1b466db2877e45dc5b3923d38aec8
Kartar Singh Jabanda <kartar.jabanda@remarkable.net>
2020-10-23 21:14:03 +0100

    people

---
+++ MVCDemo/Controllers/PeopleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { FirstName = "Kartar", LastName = "Singh", Age = 20 });
+            people.Add(new PersonModel { FirstName = "Tim", LastName = "Ferriss", Age = 40 });
+            people.Add(new PersonModel { FirstName = "Lionel", LastName = "Messi", Age = 32 });
+
+            return View(people);
+        }
+    }
+}
